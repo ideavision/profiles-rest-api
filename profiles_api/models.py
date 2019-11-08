91fe864ad532ea8c1e761bd8c4e548271c38d534
ideavision <ideaxvision@gmail.com>
2019-11-08 19:09:45 +0400

    model v1

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_activate = models.BooleanFields(default=True)
