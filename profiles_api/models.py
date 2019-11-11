cf5ae7a3df2b0f0cb5774b5a35bbad5b1f7a4c7d
ideavision <ideaxvision@gmail.com>
2019-11-11 13:10:51 +0400

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,9 +2,49 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Managee for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """reate a new user profile"""
+
+
+
+
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_activate = models.BooleanFields(default=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects= UserProfileManager()
+
+    # Django admin and Django authentication system
+    
+    #provide email and password
+    USERNAME_FIELD='email' 
+    REQUIRED_FIELDS=['name']
+
+    def get_full_name(self):
+        """ Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """ Retrieve short name of user"""  
+        return self.name
+
+    def __str__(self):
+        """Retrieve string representation of our user"""
+        return self.email
+
+
+     
+
+
+     
